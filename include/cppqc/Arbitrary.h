90ef7543c5c3e44764e02b187b54028ca4cf6991
Gerard Basler <gerard.basler@gmail.com>
2012-12-03 22:55:09 +0100

    Fix VS 2010 build.

--- include/cppqc/Arbitrary.h
+++ include/cppqc/Arbitrary.h
@@ -39,18 +39,22 @@
 
 namespace cppqc {
 
-// helper class template, since default function parameters not allowed
+// helper class template, since default function parameters are not allowed
 // needed to get rid of Visual Studio compiler warnings
 	template<class Integral, bool isSigned = std::numeric_limits<Integral>::is_signed>
 	struct signedAware {
 		static Integral randomNegate(RngEngine &rng, Integral);
 
+		static void doWeirdThing(Integral, std::vector<Integral> &);
 	};
 
 	template<class Integral>
 	struct signedAware<Integral, false> {
 		static Integral randomNegate(RngEngine &rng, Integral i) {
 			return i;
+		}
+
+		static void doWeirdThing(Integral i, std::vector<Integral> &ret) {
 		}
 	};
 
@@ -61,6 +65,11 @@ namespace cppqc {
 				return -i;
 			else
 				return i;
+		}
+
+		static void doWeirdThing(Integral x, std::vector<Integral> &ret) {
+			if (x < 0 /*&& -x > x*/) // TODO: this does not make any sense to me...
+				ret.push_back(-x);
 		}
 	};
 
@@ -107,12 +116,16 @@ std::vector<T> shrinkNothing(const T &x)
     return std::vector<T>();
 }
 
+/**
+ * @return vector of shrinkies,
+ * 		   e.g, x = 10 -> {0, 5, 8, 9},   
+ * 		   x = -10 -> {10, 0, 5, 8, 9} 
+ */
 template<class Integral>
 std::vector<Integral> shrinkIntegral(Integral x)
 {
     std::vector<Integral> ret;
-    if (x < 0 && -x > x) // TODO: this does not make any sense to me...
-        ret.push_back(-x);
+	signedAware<Integral>::doWeirdThing(x, ret);
     for (Integral n = x; n != 0; n /= 2)
         ret.push_back(x - n);
     return ret;
@@ -372,6 +385,73 @@ template<class T1, class T2>
 const typename Arbitrary<std::pair<T1, T2> >::shrinkType
 ArbitraryImpl<std::pair<T1, T2> >::shrink = shrinkPair<std::pair<T1, T2> >;
 
+// this was in Arbitrary.cpp but dynamic linking with VS 2010 does not seem to work
+// so moved into header file, see http://stackoverflow.com/questions/1553854/template-static-variable
+
+const Arbitrary<bool>::unGenType ArbitraryImpl<bool>::unGen = arbitraryBool;
+const Arbitrary<bool>::shrinkType ArbitraryImpl<bool>::shrink = shrinkBool;
+
+const Arbitrary<signed char>::unGenType ArbitraryImpl<signed char>::unGen =
+	arbitrarySizedBoundedIntegral<signed char>;
+const Arbitrary<signed char>::shrinkType ArbitraryImpl<signed char>::shrink =
+	shrinkIntegral<signed char>;
+
+const Arbitrary<unsigned char>::unGenType ArbitraryImpl<unsigned char>::unGen =
+	arbitrarySizedBoundedIntegral<unsigned char>;
+const Arbitrary<unsigned char>::shrinkType
+	ArbitraryImpl<unsigned char>::shrink = shrinkIntegral<unsigned char>;
+
+const Arbitrary<signed short>::unGenType ArbitraryImpl<signed short>::unGen =
+	arbitrarySizedBoundedIntegral<signed short>;
+const Arbitrary<signed short>::shrinkType ArbitraryImpl<signed short>::shrink =
+	shrinkIntegral<signed short>;
+
+const Arbitrary<unsigned short>::unGenType
+	ArbitraryImpl<unsigned short>::unGen =
+	arbitrarySizedBoundedIntegral<unsigned short>;
+const Arbitrary<unsigned short>::shrinkType
+	ArbitraryImpl<unsigned short>::shrink = shrinkIntegral<unsigned short>;
+
+const Arbitrary<signed int>::unGenType ArbitraryImpl<signed int>::unGen =
+	arbitrarySizedBoundedIntegral<signed int>;
+const Arbitrary<signed int>::shrinkType ArbitraryImpl<signed int>::shrink =
+	shrinkIntegral<signed int>;
+
+const Arbitrary<unsigned int>::unGenType ArbitraryImpl<unsigned int>::unGen =
+	arbitrarySizedBoundedIntegral<unsigned int>;
+const Arbitrary<unsigned int>::shrinkType ArbitraryImpl<unsigned int>::shrink =
+	shrinkIntegral<unsigned int>;
+
+const Arbitrary<signed long>::unGenType ArbitraryImpl<signed long>::unGen =
+	arbitrarySizedBoundedIntegral<signed long>;
+const Arbitrary<signed long>::shrinkType ArbitraryImpl<signed long>::shrink =
+	shrinkIntegral<signed long>;
+
+const Arbitrary<unsigned long>::unGenType ArbitraryImpl<unsigned long>::unGen =
+	arbitrarySizedBoundedIntegral<unsigned long>;
+const Arbitrary<unsigned long>::shrinkType ArbitraryImpl<unsigned long>::shrink =
+	shrinkIntegral<unsigned long>;
+
+const Arbitrary<float>::unGenType ArbitraryImpl<float>::unGen =
+	arbitrarySizedReal<float>;
+const Arbitrary<float>::shrinkType ArbitraryImpl<float>::shrink = shrinkReal<float>;
+
+const Arbitrary<double>::unGenType ArbitraryImpl<double>::unGen =
+	arbitrarySizedReal<double>;
+const Arbitrary<double>::shrinkType ArbitraryImpl<double>::shrink = shrinkReal<double>;
+
+const Arbitrary<long double>::unGenType ArbitraryImpl<long double>::unGen =
+	arbitrarySizedReal<long double>;
+const Arbitrary<long double>::shrinkType ArbitraryImpl<long double>::shrink =
+	shrinkReal<long double>;
+
+const Arbitrary<char>::unGenType ArbitraryImpl<char>::unGen = arbitraryChar;
+const Arbitrary<char>::shrinkType ArbitraryImpl<char>::shrink = shrinkChar;
+
+const Arbitrary<wchar_t>::unGenType ArbitraryImpl<wchar_t>::unGen =
+	arbitraryBoundedIntegral<wchar_t>;
+const Arbitrary<wchar_t>::shrinkType ArbitraryImpl<wchar_t>::shrink =
+	shrinkIntegral<wchar_t>;
 
 }
 
